c98aa7ea2fe2c04aac241f508f9fe39a89fc6216
余定邦 <876633022@qq.com>
2018-03-27 18:02:54 +0800

    add

---
+++ src/core/impl/TipsGetterImpl.java
@@ -0,0 +1,13 @@
+package core.impl;
+
+import core.interfaces.TipsGetter;
+
+public class TipsGetterImpl implements TipsGetter{
+
+	@Override
+	public String getTips() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+}
